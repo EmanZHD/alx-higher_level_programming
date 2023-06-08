25629d52d0551ad1fee984141aad6c5dc059b096
EmaneZHD <zahidimane1@gmail.com>
2023-06-08 19:06:03 +0000

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 
+tot = 0
 if __name__ == "__main__":
     args = len(argv)
-    if args == 1:
-        print("{}".format(args - 1), "arguments.")
-    elif args == 2:
-        print("{}".format(args - 1), "argument:")
-        for i in range(1, args):
-            print("{}".format(i) + ":", argv[i])
-    elif args > 2:
-        print("{}".format(args - 1), "arguments:")
-        for i in range(1, args):
-            print("{}".format(i) + ":", argv[i])
+    for i in range(1, args):
+        tot = tot + int(argv[i])
+    if args > 1:
+        print(tot)
+    else:
+        print(0)
